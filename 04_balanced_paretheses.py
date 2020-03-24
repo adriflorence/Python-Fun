6f644ffa952400d82d28a0cf9aeed89b029ce2df
Adri Forczek <forczekadri@gmail.com>
2020-03-24 23:09:25 +0000

    chore: balances parentheses task setup

---
+++ 04_balanced_paretheses.py
@@ -0,0 +1,41 @@
+# Take a string as an input and return `True` if it's parentheses are balanced or `False` if it is not. 
+
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def size(self):
+        return len(self.items)
+    
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.size()==0:
+            return None
+        else:
+            return self.items.pop()
+
+def equation_checker(equation):
+    """
+    Check equation for balanced parentheses
+
+    Args:
+       equation(string): String form of equation
+    Returns:
+       bool: Return if parentheses are balanced or not
+    """
+    
+    
+    # TODO: Intiate stack object
+    
+    # TODO: Interate through equation checking parentheses
+    
+    # TODO: Return True if balanced and False if not
+    
+    pass
+
+# TEST CASES
+
+print ("Pass" if (equation_checker('((3^2 + 8)*(5/2))/(2+6)')) else "Fail")
+print ("Pass" if not (equation_checker('((3^2 + 8)*(5/2))/(2+6))')) else "Fail")
