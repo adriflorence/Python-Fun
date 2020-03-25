b155f99e7e9ab1670903d0f4c492a220b1d348cf
Adri Forczek <forczekadri@gmail.com>
2020-03-25 23:56:13 +0000

    balances parentheses stack solution

--- 04_balanced_paretheses.py
+++ 04_balanced_paretheses.py
@@ -17,25 +17,23 @@ class Stack:
             return self.items.pop()
 
 def equation_checker(equation):
-    """
-    Check equation for balanced parentheses
+    # Intiate stack object
+    stack = Stack()
 
-    Args:
-       equation(string): String form of equation
-    Returns:
-       bool: Return if parentheses are balanced or not
-    """
+    # Interate through equation checking parentheses
+    for c in equation:
+      if c == '(':
+        stack.push(c)
+      elif c == ')':
+        if stack.pop() == None:
+          return False
     
-    
-    # TODO: Intiate stack object
-    
-    # TODO: Interate through equation checking parentheses
-    
-    # TODO: Return True if balanced and False if not
-    
-    pass
+    # Return True if balanced and False if not
+    if stack.size() == 0:
+      return True
+    else:
+      return False
 
 # TEST CASES
-
 print ("Pass" if (equation_checker('((3^2 + 8)*(5/2))/(2+6)')) else "Fail")
 print ("Pass" if not (equation_checker('((3^2 + 8)*(5/2))/(2+6))')) else "Fail")
