01f8c1d7b0eb0d1dc1840731b7c3656f4a482728
Adri Forczek <forczekadri@gmail.com>
2020-05-04 23:40:05 +0100

    chore: adds nodes and edges to graph to represent 'udacity' text

--- 18_Dijkstra_algorithm.py
+++ 18_Dijkstra_algorithm.py
@@ -28,3 +28,44 @@ class Graph(object):
         if node1 in self.nodes and node2 in self.nodes:
             node1.remove_child(node2)
             node2.remove_child(node1)
+
+
+# ADDING GRAPH NODES AND EDGES
+
+node_u = GraphNode('U')
+node_d = GraphNode('D')
+node_a = GraphNode('A')
+node_c = GraphNode('C')
+node_i = GraphNode('I')
+node_t = GraphNode('T')
+node_y = GraphNode('Y')
+
+graph = Graph([node_u, node_d, node_a, node_c, node_i, node_t, node_y])
+graph.add_edge(node_u, node_a, 4)
+graph.add_edge(node_u, node_c, 6)
+graph.add_edge(node_u, node_d, 3)
+graph.add_edge(node_d, node_u, 3)
+graph.add_edge(node_d, node_c, 4)
+graph.add_edge(node_a, node_u, 4)
+graph.add_edge(node_a, node_i, 7)
+graph.add_edge(node_c, node_d, 4)
+graph.add_edge(node_c, node_u, 6)
+graph.add_edge(node_c, node_i, 4)
+graph.add_edge(node_c, node_t, 5)
+graph.add_edge(node_i, node_a, 7)
+graph.add_edge(node_i, node_c, 4)
+graph.add_edge(node_i, node_y, 4)
+graph.add_edge(node_t, node_c, 5)
+graph.add_edge(node_t, node_y, 5)
+graph.add_edge(node_y, node_i, 4)
+graph.add_edge(node_y, node_t, 5)
+
+# IMPLEMENTATION
+
+import math
+
+def dijkstra(start_node, end_node):
+    pass
+
+
+print('Shortest Distance from {} to {} is {}'.format(node_u.value, node_y.value, dijkstra(node_u, node_y)))
